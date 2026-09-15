aa92ec9a58bf42b11bbf458c6db4d177f6806cbd
promaaa <promaadev@proton.me>
2026-09-15 10:34:07 +0900

    chore(sync): resolve machine conflicts and synchronize configs with archy

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -27,5 +27,5 @@ require("default.hypr.toggles")
 
 -- Add any other personal Hyprland configuration below.
 -- o.window("qemu", { workspace = "5" })
-o.window("^(nz.co.mega.megasync|megasync)$", { tag = "+floating-window" })
+o.window("^(MEGAsync|nz.co.mega.megasync|megasync)$", { float = true, center = true, tag = "+floating-window" })
 o.window("^(kakaotalk\\.exe|KakaoTalk\\.exe)$", { float = true })
